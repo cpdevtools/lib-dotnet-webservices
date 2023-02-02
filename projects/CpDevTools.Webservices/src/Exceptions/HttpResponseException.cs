91b545d31b536763b64faaf35246c2bd5f33ccd7
Dan Erdelt <erd@conceptual.ca>
2023-02-02 03:46:08 +0000

    ...

--- projects/CpDevTools.Webservices/src/Exceptions/HttpResponseException.cs
+++ projects/CpDevTools.Webservices/src/Exceptions/HttpResponseException.cs
@@ -6,14 +6,12 @@ namespace CpDevTools.Webservices.Exceptions
     public int StatusCode { get; }
     public object? Details { get; }
 
-    public HttpResponseException(int statusCode, object? details = null) : this(statusCode, null, details) { }
+        public HttpResponseException(int statusCode, object? details = null) : this(statusCode, null, details) { }
 
     public HttpResponseException(int statusCode, string? message = null, object? details = null) : base(message)
     {
       StatusCode = statusCode;
       Details = details;
     }
-
-
   }
 }
